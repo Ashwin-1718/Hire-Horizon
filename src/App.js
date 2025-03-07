28288ec51123222957e7312d64f7b427c0d1aad3
Ashwin-1718 <aayadav371@gmail.com>
2025-03-07 20:22:50 +0530

    Commit
    Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Steps from './components/Steps';
-import VideoSection from './components/VideoSection';
+// import VideoSection from './components/VideoSection';
 import JobListings from './components/JobListings';
 import JobApplication from './components/JobApplication'; 
 import Footer from './components/Footer';
