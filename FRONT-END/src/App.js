8d731a91b733775ba3c6a7f5e33022b162a37796
Ashwin-1718 <aayadav371@gmail.com>
2025-05-01 20:32:09 +0530

    commmt

--- FRONT-END/src/App.js
+++ FRONT-END/src/App.js
@@ -23,7 +23,7 @@ const App = () => {
           <Navbar />
           <main>
             <Routes>
-              <Routeq
+              <Route
                 path="/"
                 element={
                   <>
